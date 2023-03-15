1861eeb118d34d00359475d57c1ca3009c5d254d
TehVilly <andrewkim2357@gmail.com>
2023-03-15 15:14:15 +0900

    Both tasks done

--- func.py
+++ func.py
@@ -6,24 +6,45 @@ def add(x, y):
 #       neg() should return the negation of the given number, and sqrt() should
 #       return the square root of the given number. 
 
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    return x / y
+
+def mult(x, y):
+    return x * y
+
+def exp(x, y):
+    return x ** y 
+
 def neg(x):
-    ... # fill here
+    return -x
 
 def sqrt(x):
-    ... # fill here
+    return x ** 0.5
 
 # -------------------------------------- Task 2 -----------------------------------
 
 # TODO: Implement the quadratic formula using *only* the functions defined here.
 #       Do not use arithmetic operators directly. 
 #       You're allowed to define other functions.
-a = 1
-b = -3
-c = 1
+a = 3
+b = -1
+c = 5
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
-# Note: Make sure to remove the ellipsis (...) when you're writing your code
+discriminant = sqrt(sub(exp(b, 2), mult(mult(4, a), c)))
+numerator1 = add(neg(b), discriminant)
+numerator2 = sub(neg(b), discriminant)
+denominator = mult(2, a)
+x1 = div(numerator1, denominator)
+x2 = div(numerator2, denominator)
 
-print("First root:" + str(x1))
-print("Second root:" + str(x2))
+arithx1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2*a)  
+arithx2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2*a) 
+
+print("Non Arithmetic calc - First root:" + str(x1))
+print("Non Arithmetic calc - Second root:" + str(x2))
+
+print("Arithmetic calculation:" + str(arithx1))
+print("Arithmetic calculation:" + str(arithx2))
